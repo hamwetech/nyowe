1f126ba82e3591e04529110507628356310ad2a4
Geoffrey Ndungu <geoffrey.w.ndungu@gmail.com>
2023-04-19 18:48:43 +0300

    Update age profile

--- userprofile/models.py
+++ userprofile/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.dispatch import receiver
+from datetime import datetime, date
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.contrib.auth.models import Group
