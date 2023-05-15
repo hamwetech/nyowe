c8f9227527a3e87d0700081a8eefec1bb0ce079d
Geoffrey Ndungu <geoffrey.w.ndungu@gmail.com>
2023-05-15 12:55:49 +0300

    Update

--- coop/utils.py
+++ coop/utils.py
@@ -3,16 +3,17 @@ from conf.utils import log_debug, log_error
 from coop.models import Cooperative
 
 def sendMemberSMS(request, member, message):
-    if member.cooperative.send_message:
-        msisdn = member.phone_number
-        try:
-            log_debug('Message: %s Receiver: %s' % (message, msisdn))
-            sendSMS(request, msisdn, message)
-            
-            return True
-        except Exception:
-            log_error()
-            return False
+    if member.cooperative:
+        if member.cooperative.send_message:
+            msisdn = member.phone_number
+            try:
+                log_debug('Message: %s Receiver: %s' % (message, msisdn))
+                sendSMS(request, msisdn, message)
+
+                return True
+            except Exception:
+                log_error()
+                return False
     return False
 
 
